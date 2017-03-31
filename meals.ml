862b9c20e5bb3a90aeb8a73aa1ffec956c3976b3
Tom Raithel <t.raithel@googlemail.com>
2017-03-31 10:47:14 +0200

    Text formatting

--- meals.ml
+++ meals.ml
@@ -1,9 +1,22 @@
 open Soup;;
 open Core.Std;;
 
-type day = {
-  name: string;
-};;
+let rec trim s =
+  let l = String.length s in
+  if l=0 then s
+  else if s.[0]=' ' || s.[0]='\t' || s.[0]='\n' || s.[0]='\r' then
+    trim (String.sub s 1 (l-1))
+  else if s.[l-1]=' ' || s.[l-1]='\t' || s.[l-1]='\n' || s.[l-1]='\r' then
+    trim (String.sub s 0 (l-1))
+  else
+    s
+;;
+
+let trimmed_leaf_text node =
+  node
+  |> R.leaf_text
+  |> trim
+;;
 
 let get_meals soup =
   soup $$ ".meal"
@@ -12,12 +25,11 @@ let get_meals soup =
 let get_meal_titles meal =
   let titles = meal $$ ".meal-row .title" in
   let titles_list = to_list titles in
-  let titles_string_list = List.map ~f:R.leaf_text titles_list in
-  String.concat ~sep:"+" titles_string_list
+  List.map ~f:trimmed_leaf_text titles_list
 ;;
 
 let get_meal_day meal =
-  meal $ ".week-day span" |> R.leaf_text
+  meal $ ".week-day span" |> trimmed_leaf_text
 ;;
 
 let get_day meal =
@@ -27,11 +39,16 @@ let get_day meal =
   )
 ;;
 
-let get_titles filename =
+let get_menu filename =
   let soup = read_file filename |> parse in
   let meals_list = to_list (get_meals soup) in
-  let titles = List.map ~f:get_day meals_list in
-  String.concat ~sep:"+" (List.map ~f:(fun (day, title) -> day ^ title) titles)
+  List.map ~f:get_day meals_list
 ;;
 
-print_endline (get_titles "./index.html");;
+let render_titles titles = String.concat ~sep:"\n - " titles;;
+
+let render menu =
+  String.concat ~sep:"\n-------\n" (List.map ~f:(fun (day, titles) -> day ^ (render_titles titles)) menu)
+;;
+
+print_endline (render (get_menu "./index.html"));;
