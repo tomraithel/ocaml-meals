e2319991557fc03b3e7809f21d6de5ef0cc00765
Tom Raithel <t.raithel@googlemail.com>
2017-04-04 08:36:35 +0200

    Refactor method calls

--- meals.ml
+++ meals.ml
@@ -5,9 +5,11 @@ open Cohttp
 open Cohttp_lwt_unix
 
 let fetch_html =
-  Client.get (Uri.of_string "https://meals.aoe.com/") >>= fun (resp, body) ->
-  body |> Cohttp_lwt_body.to_string >|= fun body ->
-  body
+  Client.get (Uri.of_string "https://meals.aoe.com/") >>=
+    fun (resp, body) ->
+      body
+        |> Cohttp_lwt_body.to_string >|= fun body ->
+          body
 ;;
 
 let trimmed_leaf_text node =
@@ -21,9 +23,10 @@ let get_meals soup =
 ;;
 
 let get_meal_titles meal =
-  let titles = meal $$ ".meal-row .title" in
-  let titles_list = to_list titles in
-  List.map ~f:trimmed_leaf_text titles_list
+  meal
+    |> select ".meal-row .title"
+    |> to_list
+    |> List.map ~f:trimmed_leaf_text
 ;;
 
 let get_meal_day meal =
@@ -38,9 +41,11 @@ let get_day meal =
 ;;
 
 let get_menu body =
-  let soup = body |> parse in
-  let meals_list = to_list (get_meals soup) in
-  List.map ~f:get_day meals_list
+  body
+    |> parse
+    |> get_meals
+    |> to_list
+    |> List.map ~f:get_day
 ;;
 
 let render_titles titles = String.concat ~sep:"\n - " titles;;
@@ -48,7 +53,6 @@ let render_titles titles = String.concat ~sep:"\n - " titles;;
 let render menu =
   String.concat ~sep:"\n-------\n" (List.map ~f:(fun (day, titles) -> day ^ (render_titles titles)) menu)
 ;;
-
 
 let () =
   let body = Lwt_main.run fetch_html in
